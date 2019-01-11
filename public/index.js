ae05a471b54c1c255352a3c7a60bb2770fd5ad78
User <campa813@gmail.com>
2019-01-10 20:59:55 -0600

    Removing method that generated the list on page render. It will now not display until the user provides input.

--- public/index.js
+++ public/index.js
@@ -1,25 +1,4 @@
 
-
-
-function getListOfProductNames() {
-    var startTag = "<ul>";
-    var endTag = "</ul>";
-
-    var elements = "";
-    for (var i = 0; i < products.length; i++) {
-        elements += "<li> " + products[i].name + " <button onclick="+'alert()'+">Add To Cart?</button> "
-                 + " <ul>" 
-                 + "<li> Rating: " + products[i].rating + " </li>"
-                 + "<li> Reviews: " + products[i].reviews.length + " </li>"
-                 + "<li> Price: " + products[i].price + " </li>" 
-                 + " </ul>" 
-                 + " </li>"
-    }
-    
-    return startTag + elements + endTag; 
-}
-
-
 function GetItems(toLookFor) {
     toLookFor = toLookFor.toLowerCase();
     let toReturn = [];
