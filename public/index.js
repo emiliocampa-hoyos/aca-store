318a4fba5cf2a4139312cc3e79032fd37b8ba313
User <campa813@gmail.com>
2019-01-12 12:56:36 -0600

    Added functionality to check for user activity and to display details of an item.

--- public/index.js
+++ public/index.js
@@ -1,5 +1,10 @@
+var cartItems = [];
 
-function GetItems(toLookFor) {
+if (sessionStorage.getItem("cartItems")) {
+    cartItems = JSON.parse(sessionStorage.getItem("cartItems"));
+}
+
+function GetItemsByName(toLookFor) {
     toLookFor = toLookFor.toLowerCase();
     let toReturn = [];
     let product;
@@ -15,8 +20,52 @@ function GetItems(toLookFor) {
 }
 
 
+function getProductsByCategory(toLookFor) {
+    toLookFor = toLookFor.toLowerCase();
+    let toReturn = [];
+    let product;
+
+    for (var i = 0; i < products.length; i++) {
+        product = products[i];
+        
+        if (product.category.toLowerCase() == toLookFor) {
+            toReturn.push(product);
+        }
+    }
+    return toReturn;
+}
+
+
+function GetItemById(_id) {
+   for (let i = 0; i < products.length; i++) {
+       if (products[i]._id == _id)
+        return products[i];
+   }
+}
+
+
 function addToCart(itemKey) {
-    console.log("Item: " + itemKey);
+    let productToAdd = GetItemById(itemKey);
+    cartItems.push(productToAdd);
+    sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
+    alert("Item added to cart: " + productToAdd.name);
+}
+
+
+
+function getProductCategories() {
+    let categories = [];
+
+    for (let i = 0; i < products.length; i++) {
+        if (categories.indexOf(products[i].category) == -1)
+            categories.push(products[i].category);
+    }
+    return categories;
+}
+
+
+function clearSession() {
+    sessionStorage.clear();
 }
 
 
